07b5de48c8fd207ce70cf456041c9c0f3cfc2454
Mehmet Aktas <mehmet@oredkbe204ad01.rad.rutgers.edu>
2021-03-12 21:30:28 -0500

    Did a clean-up. Added init.el for Amarel.

--- elpa/adafruit-wisdom-readme.txt
+++
@@ -1,4 +0,0 @@
-I've always enjoyed the engineering quotes found at the footer of
-each page on adafruit.com ... now you can too!  This code is
-derived from Dave Pearson's dad-joke.el, except adafruit.com
-publishes their quotes as rss so we have to deal with that.
--- elpa/silkworm-theme-20191005.1903/silkworm-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "silkworm-theme" "20191005.1903" "Light theme with pleasant, low contrast colors." '((emacs "24")) :commit "6cb44e3bfb095588aa3bdf8d0d45b583521f9e2c" :authors '(("Martin Haesler")) :maintainer '("Martin Haesler"))
