1ad386e18e6b4005b1aa78d5a7bfd653ebea79f7
Mehmet Aktas <mfatihaktas@gmail.com>
2021-12-16 18:05:39 -0500

    Updated to move my emacs setup to Overjet Mac

---
+++ elpa/paper-theme-20200510.5/paper-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from paper-theme.el  -*- no-byte-compile: t -*-
+(define-package "paper-theme" "20200510.5" "A minimal Emacs colour theme." '((emacs "24")) :commit "8a3b529d5ece261a8847298ea03ed35615cc9bfa" :authors '(("Göktuğ Kayaalp")) :maintainer '("Göktuğ Kayaalp") :keywords '("theme" "paper") :url "https://dev.gkayaalp.com/elisp/index.html#paper")
